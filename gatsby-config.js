8bb4839239fc1d85acf681ddf2fe3526704f2ade
apiepho <alpiepho@gmail.com>
2019-09-08 09:34:43 -0600

    added steps for PWA

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,6 +22,21 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `My Blog (Round 2)`,
+        short_name: `My Blog2`,
+        start_url: `/`,
+        background_color: `#0d4e8c`,
+        theme_color: `#2876C0`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-offline`,
   ],
   // Customize your site metadata:
   siteMetadata: {
