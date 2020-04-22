7e16fc70b42bacd567cc872b7cb5d55e5e632997
al <alpiepho@yahoo.com>
2020-04-22 12:59:41 -0600

    adding post for fixing emojis

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,36 +3,33 @@ module.exports = {
   plugins: [
     {
       resolve: `gatsby-theme-blog`,
-      options: {},
+      options: { 
+        mdxOtherwiseConfigured: true
+      },
     },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // should this be configurable by the end-user?
+              maxWidth: 1380,
+              linkImagesToOriginal: false,
+            },
+          },
+          { resolve: `gatsby-remark-copy-linked-files` },
+          { resolve: `gatsby-remark-smartypants` },
+        ],
+        remarkPlugins: [require(`remark-slug`), require(`remark-emoji`)],
+      },
+    },    
     {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
-          {
-            resolve: `gatsby-remark-emojis`,
-            options: {
-              // Deactivate the plugin globally (default: true)
-              active : true,
-              // Add a custom css class
-              class  : 'emoji-icon',
-              // In order to avoid pattern mismatch you can specify
-              // an escape character which will be prepended to the
-              // actual pattern (e.g. `#:poop:`).
-              escapeCharacter : '#', // (default: '')
-              // Select the size (available size: 16, 24, 32, 64)
-              size   : 64,
-              // Add custom styles
-              styles : {
-                display      : 'inline',
-                margin       : '0',
-                'margin-top' : '1px',
-                position     : 'relative',
-                top          : '5px',
-                width        : '25px'
-              }
-            }
-          },
           {
             resolve: `gatsby-remark-images`,
             options: {
