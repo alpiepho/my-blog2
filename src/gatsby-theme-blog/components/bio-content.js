def84f5ebc0fc4887aba6342fd2097a9f36357ac
apiepho <alpiepho@gmail.com>
2020-05-12 19:23:57 -0600

    add link to resume site

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -10,6 +10,6 @@ export default () => (
     Hi I'm Al!  This is my "new-and-improved" blog.
     {` `}
     <br />
-    Also visit my <Styled.a href="https://alpiepho.github.io/project-portfolio3">portfolio</Styled.a>.
+    Also visit my <Styled.a target="_blank" rel="noreferrer" href="https://alpiepho.github.io/project-portfolio3">portfolio</Styled.a>, or my  <Styled.a target="_blank" rel="noreferrer" href="https://alpiepho.github.io/css-grid-resume-mine">resume</Styled.a>.
   </Fragment>
 )
